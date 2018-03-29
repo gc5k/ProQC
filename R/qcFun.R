e6de4ddcb82370aad60156b72dde55cea5d29357
gc5k <chenguobo@gmail.com>
2018-03-29 21:27:56 +0800

    update various

--- R/qcFun.R
+++ R/qcFun.R
@@ -1,37 +1,36 @@
 #############function pca
-pEvecCat <- function(pEvec, cat, PC=c(1,2))
+pEvecCat <- function(pEg, Cat, PC=c(1,2))
 {
-  tName=names(table(cat))
-  layout(matrix(1:length(tName), floor(sqrt(length(tName))), ceiling(sqrt(length(tName)))))
-  for(i in 1:length(tName))
+  TN=names(table(Cat))
+  layout(matrix(1:length(TN), floor(sqrt(length(TN))), ceiling(sqrt(length(TN)))))
+  for(i in 1:length(TN))
   {
-    idx=which(cat == tName[i])
-    plot(pch=16, cex=0.7, main=tName[i], bty="l", xlab=paste0("Eigenvector ", PC[1]), ylab=paste0("Eigenvector ", PC[2]), pEvec[idx,PC[1]], pEvec[idx,PC[2]], xlim=range(pEvec[,PC[1]])*1.1, ylim=range(pEvec[,PC[2]])*1.1)
+    idx=which(cat == TN[i])
+    plot(pch=16, cex=0.7, main=TN[i], bty="l", xlab=paste0("Eigenvector ", PC[1]), ylab=paste0("Eigenvector ", PC[2]), pEg$vectors[idx,PC[1]], pEg$vectors[idx,PC[2]], xlim=range(pEg$vectors[,PC[1]])*1.1, ylim=range(pEg$vectors[,PC[2]])*1.1)
     abline(v=0, h=0, col="grey", lty=2)
-    points(mean(pEvec[idx,PC[1]]), mean(pEvec[idx,PC[2]]), cex=1.5, lwd=2, col="green")
+    points(mean(pEg$vectors[idx,PC[1]]), mean(pEg$vectors[idx,PC[2]]), cex=1.5, lwd=2, col="green")
   }
 }
 
-pEvecCatCov <- function(pEvec, cat, cov, m,PC=c(1,2))
+pEvecCatCov <- function(pEg, Cat, Cov, m, PC=c(1,2))
 {
 
-  tName=names(table(cat))
-  layout(matrix(1:length(tName), floor(sqrt(length(tName))), ceiling(sqrt(length(tName)))))
-  for(i in 1:length(tName))
+  TN=names(table(Cat))
+  layout(matrix(1:length(TN), floor(sqrt(length(TN))), ceiling(sqrt(length(TN)))))
+  for(i in 1:length(TN))
   {
-    idx=which(cat == tName[i])
-    colM=max(MisP/m)
-    colS=min(MisP/m)
-    plot(pch=16, cex=0.7, main=tName[i], bty="l", xlab=paste0("Eigenvector ", PC[1]), ylab=paste0("Eigenvector ", PC[2]), pEvec[idx,PC[1]], pEvec[idx,PC[2]], xlim=range(pEvec[,PC[1]])*1.1, ylim=range(pEvec[,PC[2]])*1.1, col=rgb((MisP/ncol(exDat)-colS)/colM, 1-(MisP/m-colS)/colM, 1))# (MisP/m-colS)/colM))
+    idx=which(Cat == TN[i])
+    colM=max(Cov/m)
+    colS=min(Cov/m)
+    plot(pch=16, cex=0.7, main=TN[i], bty="l", xlab=paste0("Eigenvector ", PC[1]), ylab=paste0("Eigenvector ", PC[2]), pEg$vectors[idx,PC[1]], pEg$vectors[idx,PC[2]], xlim=range(pEg$vectors[,PC[1]])*1.1, ylim=range(pEg$vectors[,PC[2]])*1.1, col=rgb((Cov/length(Cov)-colS)/colM, 1-(Cov/m-colS)/colM, 1))
 
     abline(v=0, h=0, col="grey", lty=2)
-    points(mean(pEvec[idx,PC[1]]), mean(pEvec[idx,PC[2]]), cex=1.5, lwd=2, col="black")
+    points(mean(pEg$vectors[idx,PC[1]]), mean(pEg$vectors[idx,PC[2]]), cex=1.5, lwd=2, col="black")
   }
 }
 
-pEvecPlot <- function(pEvec, PC=c(1,2), COL="grey", ma=0.3)
+pEvecPlot <- function(pEg, PC=c(1,2), COL="grey", ma=0.3)
 {
-
   pc=length(PC)
   par(mai=c(ma, ma, ma, ma))
   nt=pc*(pc-1)/2
@@ -43,7 +42,7 @@ pEvecPlot <- function(pEvec, PC=c(1,2), COL="grey", ma=0.3)
   {
     for(j in (i+1):pc)
     {
-      plot(pEvec[,PC[i]+2],pEvec[, PC[j]+2], cex=0.5, pch=16, col=COL, xlim=1.2*range(pEvec[,PC[i]+2]), ylim=1.2*range(pEvec[,PC[j]+2]), bty="l", axes = F)
+      plot(pEg$vectors[,PC[i]+2], pEg$vectors[, PC[j]+2], cex=0.5, pch=16, col=COL, xlim=1.2*range(pEg$vectors[,PC[i]+2]), ylim=1.2*range(pEg$vectors[,PC[j]+2]), bty="l", axes = F)
       abline(h=0, v=0, col="grey70", lty=2)
     }
   }
@@ -51,14 +50,14 @@ pEvecPlot <- function(pEvec, PC=c(1,2), COL="grey", ma=0.3)
   {
     if (i== 1)
     {
-      plot(x=NULL, y=NULL, xlim=1.2*range(pEvec[,PC[i]+2]), ylim=1.2*range(pEvec[,PC[i]+2]), bty="l")
+      plot(x=NULL, y=NULL, xlim=1.2*range(pEg$vectors[,PC[i]+2]), ylim=1.2*range(pEg$vectors[,PC[i]+2]), bty="l")
     } else if (i == pc)
     {
-      plot(x=NULL, y=NULL, xlim=1.2*range(pEvec[,PC[i]+2]), ylim=1.2*range(pEvec[,PC[i]+2]), bty="l")
+      plot(x=NULL, y=NULL, xlim=1.2*range(pEg$vectors[,PC[i]+2]), ylim=1.2*range(pEg$vectors[,PC[i]+2]), bty="l")
     } else {
-      plot(x=NULL, y=NULL, xlim=1.2*range(pEvec[,PC[i]+2]), ylim=1.2*range(pEvec[,PC[i]+2]), bty="l")
+      plot(x=NULL, y=NULL, xlim=1.2*range(pEg$vectors[,PC[i]+2]), ylim=1.2*range(pEg$vectors[,PC[i]+2]), bty="l")
     }
-    text(mean(1.2*range(pEvec[,PC[i]+2])), mean(1.2*range(pEvec[,PC[i]+2])),labels = paste("EV", PC[i]))
+    text(mean(1.1*range(pEg$vectors[,PC[i]+2])), mean(1.1*range(pEg$vectors[,PC[i]+2])), labels = paste("EV", PC[i]))
   }
 }
 
@@ -93,4 +92,91 @@ proCorMatrix <- function(pMat)
 pCorMatHist <- function(pCorMat)
 {
   hist(pCorMat[row(pCorMat) < col(pCorMat)], xlab="Individual relatedness", main = "Proteomic relatedness")
+}
+
+##ProteinMissing
+proMissing <- function(pMat)
+{
+  proMiss=array(0, nrow(exDat))
+  for(i in 1:nrow(exDat))
+  {
+    proMiss[i]=length(which(is.na(exDat[i,])))
+  }
+  return(proMiss)
+}
+
+indMissing <- function(pMat)
+{
+  indMiss=array(0, ncol(exDat))
+  for(i in 1:ncol(exDat))
+  {
+    indMiss[i]=length(which(is.na(exDat[,i])))
+  }
+  return(indMiss)
+}
+
+##
+proDensityPlot <- function(pMat, TAG)
+{
+  layout(matrix(c(1,1,4,1,1,5,2,3,6), 3, 3, byrow = T))
+  for(i in 1:nrow(pMat)) {
+    dn = density(log10(as.numeric(pMat[i,!is.na(pMat[i,])])))
+    if(i == 1) {
+      plot(dn, main="All individuals", pch=16, cex=0.2, bty='l', xlab="")
+    } else {
+      lines(dn$x,dn$y, lwd=0.2)
+    }
+  }
+
+  TS = names(table(TAG))
+  for (i in 1:length(TS)) {
+    idxC=which(TAG == TS[i])
+    for (j in 1:length(idxC)) {
+      dn = density(log10(as.numeric(pMat[idxC[j],!is.na(pMat[idxC[j],])])))
+      if(j == 1) {
+        plot(dn, main=TS[i], pch=16, cex=0.2, bty='l', xlab="")
+      } else {
+        lines(dn$x,dn$y, lwd=0.2)
+      }
+    }
+  }
+}
+
+##pvca
+pVCA <- function(pEg, pc=5, Cat)
+{
+  layout(matrix(1:2, 1, 2))
+  TN=names(table(Cat))
+
+  Wt=pEg$values[1:pc]/sum(pEg$values)
+  wRsq=array(0, 3)
+  idxT=which(Cat %in% TN)
+  x=Cat[idxT]
+
+  Rsq=array(0,pc)
+  names(Rsq)=seq(1, pc)
+  for (i in 1:pc)
+  {
+    an=aov(pEg$vectors[idxT,i]~x)
+    Rsq[i]=summary(an)[[1]][1,2]/sum(summary(an)[[1]][,2])
+  }
+
+  barplot(main="", Rsq, ylim=c(0, max(Rsq)*1.2), ylab=expression(R^2), xlab="PC", border = F)
+  barplot(Rsq*Wt, ylim=c(0, max(Rsq*Wt)*1.2), ylab=expression(paste("Weighted ", R^2, " (PVCA)")), xlab="PC", border = F)
+}
+
+###
+pcaCatPlot <- function(pEg, eve = 5, Cat)
+{
+  TB = names(table(Cat))
+  vA = matrix(0, length(TB), eve)
+  rownames(vA) = TB
+  for(i in 1:eve)
+  {
+    for(j in 1:length(TB))
+    {
+      vA[j,i] = var(pEg$vectors[which(Cat==TB[j]),i])
+    }
+  }
+  barplot(t(vA), beside = T, border = F, ylab="Variation")
 }
